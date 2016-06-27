f33058e6a254a611753214812b305922aa0b797a
chenjj <chenjj@yunhuni.com>
2016-06-27 16:55:36 +0800

    充值页面

---
+++ js/cost/order.js
@@ -0,0 +1,26 @@
+$(function () {
+  $('.lastMonth ').datepicker({
+    language: "zh-CN",
+    format: 'yyyy-mm',
+    startView: "months",
+    minViewMode: "months",
+    todayHighlight: true,
+    todayBtn: true,
+    autoclose: true //fail
+  }).on('changeDate',function(e){
+    $(this).datepicker('hide')
+  });
+
+  $('.currentMonth ').datepicker({
+    language: "zh-CN",
+    format: 'yyyy-mm',
+    startView: "months",
+    minViewMode: "months",
+    todayHighlight: true,
+    todayBtn: true,
+    autoclose: true //fail
+  }).on('changeDate',function(e){
+    $(this).datepicker('hide')
+  });
+  
+});
--- js/cost/recharge.js
+++ js/cost/recharge.js
@@ -1,34 +1,34 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $('#costDefulat').bootstrapValidator({
         message: '',
-        feedbackIcons: {
-        },
-        submitHandler: function(){
-            alert(2);return false;
+        feedbackIcons: {},
+        submitHandler: function () {
         },
         fields: {
-            price: {
-                selector:'.costprice',
-                message: '',
-                validators: {
-                    notEmpty: {
-                        message: '请输入正确的金额'
-                    },
-                    regexp: {
-                        regxp: /^[0-9]{1,20}$/,
-                        message: '用户名必须大于6，小于25个字符'
-                    }
-                }
-            },
-            radiovalid:{
-                selector:'.radiovalid',
-                message:'请选择支付方式',
+            radiovalid: {
+                selector: '.radiovalid',
+                message: '请选择支付方式',
                 validators: {
                     notEmpty: {
                         message: '请选择支付方式'
                     }
                 }
+            },
+            price: {
+                selector: '.costprice',
+                message: '请输入正确的金额',
+                validators: {
+                    notEmpty: {
+                        message: '充值金额不能为空'
+                    },
+                    regexp: {
+                        //regexp: /^[0-9]+([.]{1}[0-9]+){0,1}$/,
+                        regexp: /^([1-9]\d*(\.\d*[1-9])?)|(0\.\d[1-9]{0,1})$/,
+                        message: '请输入正确的金额'
+                    }
+                }
             }
+
         }
     });
 });
