a3b7ac7e16249dcb3022cfec079831e22a487654
chenjj <chenjj@yunhuni.com>
2016-06-28 11:57:58 +0800

    申请应用

--- js/application/list.js
+++ js/application/list.js
@@ -2,13 +2,69 @@ $(document).ready(function() {
     //或
     $('.tabtarget').click(function(){
         var id = $(this).attr('data-toggle');
-        console.log(id);
+        cleanModal();
+        showBox();
     });
 
     //初始化modal 数据
-    function cleanmodal(){
-            
+    function cleanModal(){
+        hideSpot();
+        hideModal();
     }
-    
+
+    function hideSpot(){
+        var index = 0;
+        $('.spot').each(function () {
+            if(index==0)
+                $(this).addClass('s-active');
+            else
+                $(this).removeClass('s-active');
+            index++;
+        });
+    }
+
+    function hideModal(){
+        var index = 0;
+        $('.tabModalBtn').each(function () {
+            if(index==0)
+                $(this).show();
+            else
+                $(this).hide();
+            index++;
+        });
+    }
+
+
+    //页面切换
+    $('.tabModalBtn').click(function(){
+        var id = $(this).attr('data-id');
+        var spotindex = 0;
+        $('.spot').each(function () {
+            if(spotindex==id){
+               $(this).addClass('s-active');
+            }
+            spotindex++;
+        });
+        var contentindex = 0;
+        $('.contentModal').each(function () {
+            if(contentindex==id)
+                $(this).show();
+            else
+                $(this).hide();
+            contentindex++;
+        });
+    });
+
+
+
+    $('.modalCancel').click(function(){
+        $('#mobilebox').fadeOut();
+        $('#show-bg').fadeOut();
+    });
+
+    function showBox(){
+        $('#mobilebox').fadeIn();
+        $('#show-bg').fadeIn();
+    }
     
 });
