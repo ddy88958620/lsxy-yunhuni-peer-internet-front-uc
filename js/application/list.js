9339b7ff4dad7715479896445367f059ac144114
chenjj <chenjj@yunhuni.com>
2016-06-28 09:06:40 +0800

    应用管理

---
+++ js/application/list.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    //或
+    $('.tabtarget').click(function(){
+        var id = $(this).attr('data-toggle');
+        console.log(id);
+    });
+
+    //初始化modal 数据
+    function cleanmodal(){
+            
+    }
+    
+    
+});
