4e55f64bf21ca8fb98aa49de29ec92e52db33433
chenjj <chenjj@yunhuni.com>
2016-06-29 19:56:29 +0800

    应用详情

---
+++ js/application/detail.js
@@ -0,0 +1,90 @@
+$(document).ready(function() {
+    //或
+    $('.tabtarget').click(function(){
+        var id = $(this).attr('data-toggle');
+        cleanModal();
+        showBox();
+    });
+
+    //初始化modal 数据
+    function cleanModal(){
+        hideSpot();
+        hideModal();
+    }
+
+    function hideSpot(){
+        var index = 0;
+        $('.spot').each(function () {
+            if(index==0)
+                $(this).addClass('s-active');
+            else
+                $(this).removeClass('s-active');
+            index++;
+        });
+    }
+
+    function hideModal(){
+        var index = 0;
+        $('.contentModal').each(function () {
+            if(index==0)
+                $(this).show();
+            else
+                $(this).hide();
+            index++;
+        });
+    }
+
+
+    //页面切换
+    $('.tabModalBtn').click(function(){
+        var id = $(this).attr('data-id');
+        var spotindex = 0;
+        $('.spot').each(function () {
+            if(spotindex==id){
+                $(this).addClass('s-active');
+            }
+            spotindex++;
+        });
+        var contentindex = 0;
+        $('.contentModal').each(function () {
+            if(contentindex==id)
+                $(this).show();
+            else
+                $(this).hide();
+            contentindex++;
+        });
+    });
+
+
+
+    $('.modalCancel').click(function(){
+        var id = $(this).attr('data-id');
+        $('#modal'+id).fadeOut();
+        $('#show-bg').fadeOut();
+    });
+
+    $('.modalShow').click(function(){
+        var id = $(this).attr('data-id');
+        
+        $('#modal'+id).fadeIn();
+        $('#show-bg').fadeIn();
+    });
+
+    $.fn.datepicker.defaults.autoclose = true;
+
+    $('.datepicker').datepicker({
+        language: "zh-CN",
+        setDate: '10/2015',
+        format: 'yyyy-mm-dd',
+        viewMode: "days",
+        minViewMode: "days",
+        todayHighlight: true,
+        todayBtn: true,
+        autoclose: true //fail
+    }).on('changeDate',function(e){
+        $(this).datepicker('hide')
+    });
+
+});
+
+
