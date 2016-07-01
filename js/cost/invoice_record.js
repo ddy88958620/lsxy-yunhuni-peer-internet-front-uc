927b86866a2e59fa5c8e1edf561bfa48c150a8da
chenjj <chenjj@yunhuni.com>
2016-07-01 10:18:45 +0800

    发票页面

--- js/cost/invoice_record.js
+++ js/cost/invoice_record.js
@@ -10,7 +10,42 @@ $(function () {
         todayHighlight: true,
         todayBtn: true,
         autoclose: true //fail
-    }).on('changeDate',function(e){
+    }).on('changeDate', function (e) {
         $(this).datepicker('hide')
     });
+
+    
+
 });
+
+
+$('#sendinvoice').click(function () {
+    //0 填写  1，2 大于100 3大于1000
+    var type = $('.invoice-type').attr('data-type');
+    var currentprice = parseInt($('#invoice-price').html());
+    switch (type) {
+        case '1':
+        case '2':
+            if(currentprice<100){
+                tips('个人增值税普通发票、企业增值税普通发票(100元起),才能开具'); return false;
+            }
+            break;
+        case '3':
+            if(currentprice<1000){
+                tips('企业增值税专用发票(1000元起),才能开具');return false;
+            }
+            break;
+        default:
+            tips('您还未填写发票信息，请先填写完成，再进行开票申请');return false;
+            break;
+    }
+
+});
+
+
+function tips(title) {
+    bootbox.setDefaults("locale","zh_CN");
+    bootbox.alert(title, function() {
+    });
+}
+
