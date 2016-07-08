32e0a233993c08d30bb2062081a9c7924335c624
chenjj <chenjj@yunhuni.com>
2016-07-08 18:01:14 +0800

    消费记录

--- js/cost/history.js
+++ js/cost/history.js
@@ -37,4 +37,33 @@ $(function () {
     $( ".currentMonth" ).datepicker( "setDate", str);
     $( ".lastMonth" ).datepicker( "setDate", secondMonth);
   })
+
+  $('#findform').click(function(){
+    $('.tips-error').html();
+    var starttime = $('.currentMonth').val();
+    var lastMonth = $('.lastMonth').val();
+    var tips = '';
+    tips = compareTime(starttime,lastMonth);
+    if(tips){
+      $('.tips-error').html(tips); return ;
+    }
+    document.getElementById('command').submit();
+  });
+
 });
+
+
+function compareTime(starttime,endtime){
+  if(!starttime){
+    return '请填写开始时间';
+  }
+  if(!endtime){
+    return '请填写结束时间';
+  }
+  var d1 = new Date(starttime.replace(/\-/g, "\/"));
+  var d2 = new Date(endtime.replace(/\-/g, "\/"));
+  if(d1>d2){
+    return '时间范围填写有误';
+  }
+  return '';
+}
