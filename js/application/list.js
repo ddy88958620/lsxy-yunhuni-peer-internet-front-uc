fceac71fc5a82cf7b0ee29f8fa3175e27644fd96
chenjj <chenjj@yunhuni.com>
2016-07-14 10:25:19 +0800

    应用列表

--- js/application/list.js
+++ js/application/list.js
@@ -2,94 +2,27 @@ $(document).ready(function() {
 
 
 
-    //页面切换ivr
-    $('#tabModalBtn').click(function(){
-        var id = $(this).attr('data-id');
-        var spotindex = 0;
-        var res = true;
-        $('.spot').each(function () {
-            if(spotindex==id){
-               if(id==1){
-                   creatIVR();
-               }
-               if(id==3){
-                   var result = readbook();
-                   if(result==false){
-                       showtoast('请先阅读IVR协议');
-                       res = false; return;
-                   }
-                   //监听支付状态
-                   var result = syncpay();
-                   if(result==false){
-                       res = false; return;
-                   }
-               }
-                $(this).addClass('s-active');
-            }
-            spotindex++;
-        });
-        if(res==true){
-            var contentindex = 0;
-            $('.contentModal').each(function () {
-                if(contentindex==id)
-                    $(this).show();
-                else
-                    $(this).hide();
-                contentindex++;
-            });
-        }
-    });
-
-
-
-   
-    $('.tabModalBtn-2').click(function(){
-        var id = $(this).attr('data-id');
-        var spotindex = 0;
-        var res = true;
-        $('.sopt-2').each(function () {
-            if(spotindex==id){
-                $(this).addClass('s-active');
-            }
-            spotindex++;
-        });
-        if(res==true){
-            var contentindex = 0;
-            $('.contentModal-2').each(function () {
-                if(contentindex==id)
-                    $(this).show();
-                else
-                    $(this).hide();
-                contentindex++;
-            });
-        }
-    });
 
 
 });
 
 
 //初始化modal 数据
-function cleanModal(){
+function cleanModal(type){
     $('#modal-appid').val('');
     hideSpot();
-    hideModal();
+    hideModal(type);
 }
 
 function hideSpot(){
-    var index = 0;
     $('.spot').each(function () {
-        if(index==0)
-            $(this).addClass('s-active');
-        else
-            $(this).removeClass('s-active');
-        index++;
+         $(this).removeClass('s-active');
     });
 }
 
-function hideModal(){
+function hideModal(type){
     var index = 0;
-    $('.contentModal').each(function () {
+    $('#mobilebox-'+type+' .contentModal').each(function () {
         if(index==0)
             $(this).show();
         else
@@ -100,7 +33,6 @@ function hideModal(){
 
 
 $('.modalCancel').click(function(){
-
     var type = $(this).attr('data-type');
     $('#mobilebox-'+type).fadeOut();
     $('#show-bg').fadeOut();
@@ -112,9 +44,7 @@ function showBox(type){
 }
 
 function modalAction(index){
-    $('.spot').each(function () {
-        $(this).removeClass('s-active');
-    });
+    hideSpot();
     $('.spot').each(function () {
         if($(this).attr('data-action')<=index)
             $(this).addClass('s-active');
