610f1b1f2a348d9f35488ea44e12fad1ed556c87
liups <liups@yunhuni.com>
2016-07-30 10:38:50 +0800

    修改金额校验JS、

--- js/cost/recharge.js
+++ js/cost/recharge.js
@@ -21,15 +21,24 @@ $(document).ready(function () {
                     notEmpty: {
                         message: '充值金额不能为空'
                     },
-                    regexp: {
+                    callback: {
                         //regexp: /^[0-9]+([.]{1}[0-9]+){0,1}$/,
-                        regexp: /^(([1-9][0-9]*)|(([0]\.\d{1,2}|[1-9][0-9]*\.\d{1,2})))$/
-                    },
-                    callback:{
+                        // regexp: /^(([1-9][0-9]*)|(([0]\.\d{1,2}|[1-9][0-9]*\.\d{1,2})))$/,
+                        message: '请输入正确的金额',
                         callback: function(value, validator) {
-                           return Number(value) > 0;
+                        	//为了不触发多个提示，所以为空是返回true
+                        	if(value == "")
+                        		return true;
+                        	var reg = /^(([1-9][0-9]*)|(([0]\.\d{1,2}|[1-9][0-9]*\.\d{1,2})))$/;
+                        	var r = value.match(reg);
+                        	if(r == null){
+                        		return false;
+                        	}else{
+                        		return Number(value) > 0;
+                        	}
+                           
                         }
-                    },
+                    }
                 }
             }
 
