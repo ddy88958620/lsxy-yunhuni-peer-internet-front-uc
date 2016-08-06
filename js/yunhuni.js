a46afddb7a6f72d52d5423e60f5c84d56138bfac
chenjj <chenjj@yunhuni.com>
2016-08-06 15:04:00 +0800

    去掉自适应高度提示

--- js/yunhuni.js
+++ js/yunhuni.js
@@ -3,8 +3,6 @@ function onResize() {
   var content = document.getElementById('content')
   var h = $(document.body).height() + 'px';
   content.style.height = h;
-  alert('调试高度'+h);
-
   //var slim = document.querySelector('.slimScrollDiv')
   //slim.style.height = (window.innerHeight - 80) + 'px'
 }
