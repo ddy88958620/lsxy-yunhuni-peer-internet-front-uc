b095f0efdce2964a2bcf0274dbe07178989977f4
chenjj <chenjj@yunhuni.com>
2016-10-21 17:35:48 +0800

    呼叫中心统计

---
+++ js/statistics/call.js
@@ -0,0 +1,26 @@
+$(function () {
+    $('.currentMonth').datepicker({
+        language: "zh-CN",
+        format: 'yyyy-mm',
+        startView: "months",
+        minViewMode: "months",
+        todayHighlight: true,
+        todayBtn: true,
+        autoclose: true //fail
+    }).on('changeDate',function(e){
+        $(this).datepicker('hide')
+    });
+    $('.currentYear ').datepicker({
+        language: "zh-CN",
+        format: 'yyyy',
+        startView: "years",
+        minViewMode: "years",
+        todayHighlight: true,
+        todayBtn: true,
+        autoclose: true //fail
+    }).on('changeDate',function(e){
+        $(this).datepicker('hide')
+    });
+    initchart();
+    inittalkchart();
+});
