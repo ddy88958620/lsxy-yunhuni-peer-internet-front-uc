41f854b86a576f3062829de0d836266a72d1eec8
chenjj <chenjj@yunhuni.com>
2016-11-04 10:52:40 +0800

    添加pythons 上传静态文件到 oss

---
+++ oss/uploadoss_test.sh
@@ -0,0 +1,2 @@
+# 内网上传
+python ./oss/osscmd uploadfromdir ./dist oss://yunhuni-test-open/site --check_point=check_point_file --replace=false --check_md5=false --thread_num=5 config --id=LTAIMyBifjYatgOG --key=JltmXuqGiIPM4QkP8RGquPMW3SHZrg --host=oss-cn-beijing-internal.aliyuncs.com
