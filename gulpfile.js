85470ccb005603f3cbcbb661bbc8d1ab8bc589da
chenjj <chenjj@yunhuni.com>
2016-11-24 11:52:49 +0800

    时间插件样式

--- gulpfile.js
+++ gulpfile.js
@@ -10,27 +10,33 @@ gulp.task('default', function() {
   gulp.src('bower_components/**/*')
     .pipe(gulp.dest('./dist/bower_components'));
 
+
+
   //所有js文件
   gulp.src('js/**/*.js')
     .pipe(uglify())
     .pipe(gulp.dest('./dist/js'));
+
+  //单独css文件
+  gulp.src('js/bootstrap-datepicker/css/**/*')
+    .pipe(gulp.dest('./dist/js/bootstrap-datepicker/css'));
 
   //图片
   gulp.src('images/**/*')
     .pipe(gulp.dest('./dist/images'));
 
   //css
-  gulp.src('css/**/*')
+  gulp.src('css/!**!/!*')
     .pipe(cssmin())
     .pipe(gulp.dest('./dist/css'));
 
   //压缩样式
-  gulp.src('stylesheets/**/*.css')
+  gulp.src('stylesheets/!**!/!*.css')
     .pipe(cssmin())
     .pipe(gulp.dest('./dist/stylesheets'));
 
   //字体
-  gulp.src('fonts/**/*')
+  gulp.src('fonts/!**/!*')
     .pipe(gulp.dest('./dist/fonts'));
 
 });
